efa93ee92332e7606cef514f4bc5f74c8fba71a9
Chaoran Wang <superchaoran@gmail.com>
2015-05-05 23:10:37 -0500

    Pressing 1 to load next terrain (By destroying old terrain and loading new terrain)

--- Assets/TerrainSwitchScript.cs
+++ Assets/TerrainSwitchScript.cs
@@ -2,17 +2,25 @@
 using System.Collections;
 
 public class TerrainSwitchScript : MonoBehaviour {
-
+	int n = 2;
+	int currentIndex = 0;
 	// Use this for initialization
 	void Start () {
 		Vector3 pos = new Vector3(0, 0, 0);
 		//Instantiate(Resources.Load("Terrain0"), pos, Quaternion.identity);
-		Instantiate(Resources.Load("Terrain0"));
-		GameObject newTerrain = GameObject.FindGameObjectsWithTag ("Terrain") [0];
+		//GameObject newTerrain = GameObject.FindGameObjectsWithTag ("Terrain") [0];
+		int currentIndex = 0;
+		Instantiate(Resources.Load("Terrain"+currentIndex));
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.GetKeyDown ("1")) {
+			GameObject oldTerrain = GameObject.FindGameObjectsWithTag ("Terrain") [0];
+			Destroy(oldTerrain);
+			currentIndex = (currentIndex + 1) % 2;
+			Instantiate (Resources.Load ("Terrain" + currentIndex));
+		}
 	}
 }
