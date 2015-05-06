aac77fc2c24af596083238aa0fb48f8b5d39bba0
Chaoran Wang <superchaoran@gmail.com>
2015-05-05 23:28:06 -0500

    Fix Bug:terrain switcing loop/adding to-do list

--- Assets/TerrainSwitchScript.cs
+++ Assets/TerrainSwitchScript.cs
@@ -11,7 +11,10 @@ public class TerrainSwitchScript : MonoBehaviour {
 		//GameObject newTerrain = GameObject.FindGameObjectsWithTag ("Terrain") [0];
 		int currentIndex = 0;
 		Instantiate(Resources.Load("Terrain"+currentIndex));
-
+		/* To-DO
+		 * next should find the player position and spawn the terrain's center a bit below the player's position
+		 * make sure the center of the terrain is almost at the sea-level otherwise need to substract away the ground height
+		 * /
 	}
 	
 	// Update is called once per frame
@@ -19,7 +22,7 @@ public class TerrainSwitchScript : MonoBehaviour {
 		if (Input.GetKeyDown ("1")) {
 			GameObject oldTerrain = GameObject.FindGameObjectsWithTag ("Terrain") [0];
 			Destroy(oldTerrain);
-			currentIndex = (currentIndex + 1) % 2;
+			currentIndex = (currentIndex + 1) % n;
 			Instantiate (Resources.Load ("Terrain" + currentIndex));
 		}
 	}
