e46d3cb0afcb515b965d6b393f721d0a0c8d1b3e
unknown <slee168@siebl-4240-12.ad.uillinois.edu>
2015-05-14 12:47:05 -0500

    cant render pdf in browswer so its removed from launch bar

--- Assets/LaunchBar.cs
+++ Assets/LaunchBar.cs
@@ -46,32 +46,29 @@ public class LaunchBar : MonoBehaviour {
 		launchBarIsActive = false;
 
 
-		if (Regex.IsMatch(search, @"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$")) {
-			loadWebContent(search);
-		}
 
-		if (search == "new text") {
+
+		if (search == "new text window") {
 			Vector3 pos = lookCamera.transform.position + lookCamera.transform.forward * 3;
 			GameObject pane = Instantiate(Resources.Load("EditorPane"), pos, lookCamera.transform.rotation) as GameObject;
 			pane.GetComponentInChildren<Canvas>().worldCamera = lookCamera.GetComponent<Camera>();
-		}
-
-		if (false) {
-
+		} else if (Regex.IsMatch (search, @"([\da-z])*.txt")) {
 			string text = System.IO.File.ReadAllText (Application.dataPath + "\\" + search);
-			Debug.Log (text);
-			launchBarObject.SetActive (false);
-			launchBarField.DeactivateInputField ();
-			launchBarIsActive = false;
+			Vector3 pos = lookCamera.transform.position + lookCamera.transform.forward * 3;
+			GameObject pane = Instantiate(Resources.Load("EditorPane"), pos, lookCamera.transform.rotation) as GameObject;
+			pane.GetComponentInChildren<Canvas>().worldCamera = lookCamera.GetComponent<Camera>();
+			pane.GetComponentInChildren<EditorInputField>().text = text;
+		} else if (Regex.IsMatch(search, @"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$")) {
+			loadWebContent(search);
 		}
 	}
 
 	private void loadWebContent(string url) {
+		Debug.Log ("Web URl: " + url);
+
 		Vector3 pos = lookCamera.transform.position + lookCamera.transform.forward * 5;
 		GameObject web = Instantiate(Resources.Load("WebPane"), pos, lookCamera.transform.rotation) as GameObject;
 		UWKWebView wv = web.GetComponentInChildren<UWKWebView> ();
 		wv.LoadURL (url);
-		wv.Reload ();
-		Debug.Log ("Web URl: " + url);
 	}
 }
